613364046a3fc7adbc6680725a0c92c08df1df56
Jonathan Levin <jslevin09@gmail.com>
2020-09-03 14:28:41 +0300

    blog app

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: 'http://ec609b9853a3.ngrok.io'
+})
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,73 @@
+import React, { useContext, useEffect } from 'react'
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
+import { Context } from '../context/BlogContext'
+import { FlatList } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons';
+
+
+
+
+const IndexScreen = ({navigation}) => {
+  const { state, deleteBlogPost, getBlogPost } = useContext(Context)
+
+  useEffect(()=> {
+    getBlogPost();
+    const listener = navigation.addListener('didFocus', ()=>{
+      getBlogPost();
+    });
+    return () => {
+      listener.remove();
+    };
+  },[])
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <FlatList data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={()=> navigation.navigate('Show', {id: item.id})}>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title} - {item.id}</Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+            </TouchableOpacity>
+          )
+        }}
+      />
+    </View>
+  )
+}
+
+IndexScreen.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  }
+
+})
